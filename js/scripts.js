cc78d6261678b49e1117b59fb127f09b45cbe2da
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-05 10:08:27 -0700

    Add staging

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+function getFavThings() {
+    let favColor = document.getElementById("fav-color");
+    let favMovie = document.getElementById("fav-place");
+}
+
+
 window.onload = function() {
     document.querySelector("form").onsumbit = function() {
     }
